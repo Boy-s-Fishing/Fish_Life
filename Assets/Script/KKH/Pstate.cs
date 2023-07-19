2b8fd2533c180129bb806086f7adde5e52fad05b
kkh1773 <kkh177360@gmail.com>
2023-07-19 17:05:54 +0900

    물고기 먹기&레벨

--- Assets/Script/KKH/Pstate.cs
+++ Assets/Script/KKH/Pstate.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class Pstate : MonoBehaviour
 {
     public GameObject gameoverPanel;    // Start is called before the first frame update
+    public int level=1;
+    public int exp=0;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        levelUp();
     }
+   void levelUp(){
+    if(exp>=100){
+        level++;
+        exp-=100;
+    }
+   }
 
+    
 
     public void Dead(){
         Debug.Log("1");
