71df14abfc2b161a5f429c39ab732c7a0263306d
kkh1773 <kkh177360@gmail.com>
2023-07-20 14:22:41 +0900

    save추가

--- Assets/Script/KKH/Pstate.cs
+++ Assets/Script/KKH/Pstate.cs
@@ -9,25 +9,36 @@ public class Pstate : MonoBehaviour
     public int exp=0;
     void Start()
     {
+        StatusSave.Instance.LoadGameData();
+        level=StatusSave.Instance.data.level;
+        exp=StatusSave.Instance.data.exp;
+        StartCoroutine(status());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        levelUp();
-    }
    void levelUp(){
-    if(exp>=100){
-        level++;
-        exp-=100;
-    }
-   }
+        if(exp>=100){
+            level++;
+            exp-=100;
+            StatusSave.Instance.data.level=this.level;
+        }
 
-    
+    }
+
+    private void OnApplicationQuit() {
+        StatusSave.Instance.SaveGameData();
+    }
 
     public void Dead(){
-        Debug.Log("1");
         Time.timeScale=0;
         gameoverPanel.SetActive(true);
+    }
+
+
+    public IEnumerator status(){
+        while(true){
+            levelUp();
+            StatusSave.Instance.data.exp=this.exp;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
